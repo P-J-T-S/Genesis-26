2e5a1187ad4642c6b25fa8db666a19baf7a4c82b
Jagdish Padhi <jagdishpadhi9@gmail.com>
2026-02-05 23:24:28 +0530

    feat: Initial setup and Add utility functions

---
+++ Backend/app.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { globalErrorHandler } from './utils/globalErrorHandler.js';
+
+const app = express();
+
+// CORS configuration
+const allowedOrigins = (process.env.CORS_ORIGIN || 'http://localhost:5173').split(',');
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(null, true);
+        }
+    },
+    credentials: true,
+}));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
+
+
+// Routes
+import authRoutes from './routes/auth.route.js';
+app.use('/api/v1/auth', authRoutes);
+
+
+// error handler
+app.use(globalErrorHandler);
+
+export { app };
---
+++ Backend/src/utils/apiError.js
@@ -0,0 +1,14 @@
+class ApiError extends Error {
+    constructor(statusCode,
+        message = "something went wrong",
+        errors = [],
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.message = message;
+        this.errors = errors;
+        this.success = false;
+        this.data = null;
+    }
+}
+export { ApiError };
