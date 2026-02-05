43826cc6cac46de282938a0421b5d37dc22e13ea
Jagdish Padhi <jagdishpadhi9@gmail.com>
2026-02-06 00:10:14 +0530

    fix: Move app and index files to src

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import connectDB from './src/database/connect.js';
+import connectDB from './database/connect.js';
 import { app } from './app.js';
 
 connectDB().then(() => {
